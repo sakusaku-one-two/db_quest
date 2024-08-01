7a171056161b428097d06ea148c38348dc126601
sakusaku <oreosagi874@gmail.com>
2024-08-01 22:41:22 +0900

    init.sqlの動作確認完了　そしてlocalへのマウントをしないように設定

--- db/init.sql
+++ db/init.sql
@@ -1,10 +1,36 @@
 
-CREATE TABLE IF NOT EXISTS test_table (
+CREATE DATABASE IF NOT EXISTS quest_db;
+
+USE quest_db;
+
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(20) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    age INT NOT NULL
 );
 
-INSERT INTO test_table (name) VALUES('FIRST');
+INSERT INTO users(name,email,age) VALUES('saku','email_address',5);
+
+CREATE TABLE ganres (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT NOT NULL
+);
 
 
-SELECT * FROM test_table;
+CREATE TABLE broadcast_slots(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    description TEXT NOT NULL
+);
+
+
+CREATE TABLE programs(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    season INT DEFAULT 0,
+    epsode_no INT DEFAULT 1
+
+);
