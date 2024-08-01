c5992a3d82efb40043134e0ba06b28017b663464
sakusaku-one-two <tkkt132@gmail.com>
2024-08-01 19:19:27 +0900

    docker add

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+
+CREATE TABLE IF NOT EXISTS test_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO test_table (name) VALUES('FIRST');
+
+
+SELECT * FROM test_table;
