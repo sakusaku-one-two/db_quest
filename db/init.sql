fce867982e4f05aa335f51a91c705c464758cf49
sakusaku <oreosagi874@gmail.com>
2024-08-03 08:02:35 +0900

    sql checked

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,3 @@
-
 CREATE DATABASE IF NOT EXISTS quest_db;
 
 USE quest_db;
@@ -12,43 +11,34 @@ CREATE TABLE users (
 
 CREATE INDEX idx_user_id ON users(id);
 
-CREATE TABLE channels(
+CREATE TABLE channels (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    description TEXT,
-)
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+);
 
-
-CREATE TABLE ganres (
+CREATE TABLE genres (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description TEXT NOT NULL
 );
 
-
 CREATE TABLE channels_genres (
     channel_id INT NOT NULL,
-    ganre_id INT NOT NULL,
-    PRIMARY KEY(channel_id,ganre_id),
+    genre_id INT NOT NULL,
+    PRIMARY KEY(channel_id, genre_id),
     FOREIGN KEY (channel_id) REFERENCES channels(id),
-    FOREIGN KEY (ganre_id) REFERENCES genres(id)
+    FOREIGN KEY (genre_id) REFERENCES genres(id)
 );
 
-
-
-
-CREATE TABLE broadcast_slots(
+CREATE TABLE broadcast_slots (
     id INT AUTO_INCREMENT PRIMARY KEY,
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
     description TEXT NOT NULL
 );
 
-
-
-
-
-CREATE TABLE titles(
+CREATE TABLE titles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     description TEXT
@@ -57,49 +47,36 @@ CREATE TABLE titles(
 /*
 中間テーブル　タイトル（作品・番組）とジャンルの多対多を表現
 */
-CREATE TABLE titles_ganres(
+CREATE TABLE titles_genres (
     title_id INT NOT NULL,
-    ganre_id INT NOT NULL,
-    PRIMARY KEY(title_id,ganre_id),
+    genre_id INT NOT NULL,
+    PRIMARY KEY(title_id, genre_id),
     FOREIGN KEY (title_id) REFERENCES titles(id),
-    FOREIGN KEY (ganre_id) REFERENCES ganres(id) 
+    FOREIGN KEY (genre_id) REFERENCES genres(id)
 );
 
-
-CREATE TABLE programs(
+CREATE TABLE programs (
     title_id INT NOT NULL,
     season_no INT DEFAULT 1,
-    epsode_no INT DEFAULT 1,
+    episode_no INT DEFAULT 1,
     description TEXT,
-    PRIMARY KEY(title_id,season_no,epsode_no),
+    PRIMARY KEY(title_id, season_no, episode_no),
     FOREIGN KEY (title_id) REFERENCES titles(id)
 );
 
-
-CREATE TABLE broadcast_slots(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    start_time TIME NOT NULL,
-    end_time TIME NOT NULL,
-);
-
-CREATE TABLE broadcast_programs(
+CREATE TABLE broadcast_programs (
     id INT AUTO_INCREMENT PRIMARY KEY,
     program_id INT NOT NULL,
     channel_id INT NOT NULL,
     slot_id INT NOT NULL,
-    broadcast_date DATE NOT NULL,
+    broadcast_date DATE NOT NULL
 );
 
-
-CREATE TABLE view_logs(
-    id INT AUOT_INCREMENT PRIMARY KEY,
-    broadcast_programs_id INT NOT NULL,
+CREATE TABLE view_logs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    broadcast_program_id INT NOT NULL,
     user_id INT NOT NULL,
-    created_at DATETIME CURRENT_TIMESTAMP,
-    FOREIGN KEY(broadcast_program_id) REFERENCES broadcast_programs(id)
-    FOREIGN KEY(user_id) REFERENCES users(id)
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (broadcast_program_id) REFERENCES broadcast_programs(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
-
-
-
-
